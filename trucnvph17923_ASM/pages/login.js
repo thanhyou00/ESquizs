cc6d45f108326a681b00f07456c6b2c97bf4c1e0
thanhyou00 <truc06082000@gmail.com>
2022-01-24 22:12:38 +0700

    thanhyou00

---
+++ trucnvph17923_ASM/pages/login.js
@@ -0,0 +1,46 @@
+// module
+const app = angular.module("myapp", []);
+
+app.controller("myctrl", ($scope) => {
+  $scope.students = [
+    {
+      username: "teonv",
+      password: "iloveyou",
+      fullname: "Nguyễn Văn Tèo",
+      email: "teonv@fpt.edu.vn",
+      gender: "true",
+      birthday: "1995-12-21",
+      schoolfee: "1500000",
+      marks: "0",
+    },
+    {
+      username: "pheonv",
+      password: "iloveyou",
+      fullname: "Nguyễn Văn Chí Phèo",
+      email: "pheonv@fpt.edu.vn",
+      gender: "true",
+      birthday: "1985-10-11",
+      schoolfee: "2500000",
+      marks: "0",
+    },
+    {
+      username: "nopt",
+      password: "iloveyou",
+      fullname: "Phạm Thị Nở",
+      email: "nopt@fpt.edu.vn",
+      gender: "false",
+      birthday: "1993-05-15",
+      schoolfee: "2000000",
+      marks: "0",
+    },
+  ];
+
+  // handler login
+  $scope.submit = (user, pass) => {
+    $scope.flag = 0;
+    for (var i = 0; i < $scope.students.length; i++) {
+      if ((user === $scope.students[i].username)&&(pass === $scope.students[i].password)) $scope.flag++;
+    }
+    ($scope.flag==1)?alert("Đăng nhập thành công !"):alert("Sai tên đăng nhập hoặc mật khẩu !");
+  };
+});
