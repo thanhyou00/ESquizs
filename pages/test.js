59d6f674b361a8ca787b4d0300d1947c8ce469b6
thanhyou00 <truc06082000@gmail.com>
2022-02-19 23:49:48 +0700

    thanhyou00

--- pages/test.js
+++ pages/test.js
@@ -1,115 +1,18 @@
-app.controller("ctrlTest", function ($scope) {
-  $scope.course1 = [
-    {
-      Id: "ADAV",
-      Name: "Lập trình Android nâng cao",
-      Logo: "ADAV.svg",
-    },
-    {
-      Id: "ADBS",
-      Name: "Lập trình Android cơ bản",
-      Logo: "ADBS.svg",
-    },
-    {
-      Id: "ADTE",
-      Name: "Kiểm thử và triển khai ứng dụng Android",
-      Logo: "ADTE.svg",
-    },
-    {
-      Id: "ADUI",
-      Name: "Thiết kế giao diện trên Android",
-      Logo: "ADUI.svg",
-    },
-  ];
+app.controller("ctrlTest", function ($scope, $http) {
+  $scope.courses = [];
+  $scope.course = {
+    Id: "",
+    Name: "",
+    Logo: ""
+  }
+  // API
+  const api = 'https://620fbe2aec8b2ee2834b77d0.mockapi.io/api/courses';
+  $http.get(api) // Gửi request dạng GET lên API
+      .then(function (response) {
+          $scope.courses = response.data;
+      })
+      .catch(function (error) {
+          console.log(error);
+      });
 
-  $scope.course2 = [
-    {
-      Id: "ASNE",
-      Name: "Lập trình ASP.NET",
-      Logo: "ASNE.svg",
-    },
-    {
-      Id: "CLCO",
-      Name: "Điện toán đám mây",
-      Logo: "CLCO.svg",
-    },
-    {
-      Id: "DBAV",
-      Name: "SQL Server",
-      Logo: "DBAV.svg",
-    },
-    {
-      Id: "DBBS",
-      Name: "Cơ sở dữ liệu",
-      Logo: "DBBS.svg",
-    },
-  ];
-
-  $scope.course3 = [
-    {
-      Id: "GAME",
-      Name: "Lập trình game 2D",
-      Logo: "GAME.svg",
-    },
-    {
-      Id: "HTCS",
-      Name: "HTML5 và CSS3",
-      Logo: "HTCS.svg",
-    },
-    {
-      Id: "INMA",
-      Name: "Internet Marketing",
-      Logo: "INMA.svg",
-    },
-    {
-      Id: "JAAV",
-      Name: "Lập trình Java nâng cao",
-      Logo: "JAAV.svg",
-    },
-  ];
-  $scope.course4 = [
-    {
-      Id: "JABS",
-      Name: "Lập trình hướng đối tượng với Java",
-      Logo: "JABS.svg",
-    },
-    {
-      Id: "JSPR",
-      Name: "Lập trình JavaScript",
-      Logo: "JSPR.svg",
-    },
-    {
-      Id: "LAYO",
-      Name: "Thiết kế layout",
-      Logo: "LAYO.svg",
-    },
-    {
-      Id: "MOWE",
-      Name: "Thiết kế web cho điện thoại di động",
-      Logo: "MOWE.svg",
-    },
-  ];
-
-  $scope.course5 = [
-    {
-      Id: "PHPP",
-      Name: "Lập trình PHP",
-      Logo: "PHPP.svg",
-    },
-    {
-      Id: "PMAG",
-      Name: "Quản lý dự án với Agile",
-      Logo: "PMAG.svg",
-    },
-    {
-      Id: "VBPR",
-      Name: "Lập trình VB.NET",
-      Logo: "VBPR.svg",
-    },
-    {
-      Id: "WEBU",
-      Name: "Xây dựng trang web",
-      Logo: "WEBU.svg",
-    },
-  ];
 });
